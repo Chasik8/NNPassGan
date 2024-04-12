cb852b09e27985d86422ab714490ae38f8582485
никита часовких <hasnikita@mail.ru>
2024-04-13 00:56:07 +0300

    New run1.2

--- release.py
+++ release.py
@@ -27,7 +27,8 @@ def Run():
     G.to(dev)
     D.to(dev)
     f = open("Output.txt", 'w')
-    kol_passsord = 1
+    f_log=open("Output_log.txt", 'w')
+    kol_passsord = 10
     for i in range(kol_passsord):
         h = True
         while h:
@@ -35,21 +36,24 @@ def Run():
             x = x.to(dev)
             out = G(x)
             prow = D(out)
-            print(prow.detach().cpu().numpy()[0])
+            # print(prow.detach().cpu().numpy()[0])
             if prow.detach().cpu().numpy()[0] >= 0.5:
                 h = False
                 out_np = out.detach().cpu().numpy()
                 s = ''
+                s_log=''
                 sym = ""
                 for j in range(len(out_np)):
-                    print(out_np[j])
+                    # print(out_np[j])
                     if out_np[j] >= 0.5:
                         sym += '1'
                     else:
                         sym += '0'
                     if j % 8 == 7:
                         dig = int(sym, 2)
-                        s += chr(dig)
+                        if dig != 0:
+                            s += chr(dig)
+                        s_log+=chr(dig)
                         sym = ''
                 # for j in out_np:
                 #     jj = int(j * 256)
@@ -59,10 +63,12 @@ def Run():
                 #         s += chr(255)
                 #     else:
                 #         s += chr(jj)
-
+                f_log.write(s_log)
+                f_log.write('\n')
                 f.write(s)
                 f.write('\n')
     f.close()
+    f_log.close()
 
 
 def main():
